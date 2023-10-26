6ff95c30d0f833c439a0e3ece19acc062f579e02
RishitaParmar <143238574+RishitaParmar@users.noreply.github.com>
2023-10-26 20:06:33 +0530

    Add files via upload

---
+++ Task_3/Script_29.php
@@ -0,0 +1,8 @@
+<?php
+$a=array("a"=>"MAESTRO","b"=>"JUPITER","c"=>"ACCESS 125");
+print_r(array_reverse($a));
+
+/* Output:]
+Array ( [c] => ACCESS 125 [b] => JUPITER [a] => MAESTRO )
+*/
+?>
---
+++ Task_3/Script_53.php
@@ -0,0 +1,8 @@
+<?php
+$a=array("red","green","red","yellow");
+print_r(array_unique($a));
+
+/*output:
+Array ( [0] => red [1] => green [3] => yellow )*/
+
+?>
